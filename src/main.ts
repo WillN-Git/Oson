54733c4439686fcf12541c1c301d16abca2f3fd7
WillN-Git <wfndefo@gmail.com>
2022-12-31 21:36:00 +0100

    Initialize project

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,46 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import THREE from 'three';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let threeScene: THREE.Scene, renderer: THREE.WebGLRenderer, camera: THREE.Camera, clock: THREE.Clock;
+let screenWidth: number, screenHeight: number;
+
+let videoInput: HTMLVideoElement;
+let particles: THREE.Object3D, videoWidth: number, videoHeight: number, imageCache: HTMLImageElement;
+
+// Canvas
+const canvasElement = document.createElement("canvas");
+const canvasContext = canvasElement.getContext("2d");
+
+const classNameForLoading: string = "loading";
+
+// Audio
+let audioSource: HTMLAudioElement, audioAnalyser: AnalyserNode;
+const fftSize: number = 2048; // https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/fftSize
+const frequencyRange = {
+  bass: [20, 140],
+  lowMid: [140, 400],
+  mid: [400, 2600],
+  highMid: [2600, 5200],
+  treble: [5200, 14000],
+};
+
+
+/**
+ * Responsible to set up the scene. It's the main function
+ */
+const initialize = (): void => {
+}
+
+/**
+ * Create a perspective camera with a specified field of view, aspect ratio...
+ */
+const initializeCamera = (): void => {
+}
+
+
+/**
+ * Responsible to create a new video element and set it to autoplay
+ */
+const initializeVideo = (): void => {
+
+}
