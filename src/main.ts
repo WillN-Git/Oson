5a8822727f26ab84d236d00eb07e04ed7ce3f206
WillN-Git <wfndefo@gmail.com>
2023-01-01 14:18:46 +0100

    Draw particles on screen

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import "./style.css";
 import * as THREE from 'three';
 
 
@@ -5,13 +6,12 @@ let threeScene: THREE.Scene, renderer: THREE.WebGLRenderer, camera: THREE.Camera
 let screenWidth: number, screenHeight: number;
 
 let videoInput: HTMLVideoElement;
-let particles: THREE.Object3D, videoWidth: number, videoHeight: number, imageCache: HTMLImageElement;
+let particles: any;
+let videoWidth: number, videoHeight: number, imageCache: ImageData;
 
 // Canvas
 const canvasElement = document.createElement("canvas");
 const canvasContext = canvasElement.getContext("2d");
-
-const classNameForLoading: string = "loading";
 
 // Audio
 let audioSource: THREE.Audio, audioAnalyser: THREE.AudioAnalyser;
@@ -41,6 +41,8 @@ const initialize = (): void => {
 
   initializeCamera();
 
+  handleResize();
+
   // Check for media devices and get the user's media if available
   // @ts-ignore
   const mediaDevicesAvailable = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
@@ -58,6 +60,8 @@ const initialize = (): void => {
   } else {
     document.getElementById("message")?.classList.remove("hidden");
   }
+
+  draw();
 }
 
 /**
@@ -85,7 +89,7 @@ const initializeVideo = (): void => { // https://developer.mozilla.org/fr/docs/W
   videoInput = document.getElementById("video") as HTMLVideoElement;
   videoInput.autoplay = true;
 
-  navigator.mediaDevices.getUserMedia({audio: true, video: true})
+  navigator.mediaDevices.getUserMedia({audio: false, video: true})
     .then((stream) => {
         videoInput.srcObject = stream;
         videoInput.addEventListener("loadeddata", () => {
@@ -109,7 +113,7 @@ const initializeAudio = (): void => {
   audioSource = new THREE.Audio(audioListener);
   const audioLoader = new THREE.AudioLoader();
 
-  audioLoader.load('assets/demo1.mp3', (buffer) => {
+  audioLoader.load('assets/demo9.mp3', (buffer: THREE.AudioBuffer) => {
     audioSource.setBuffer(buffer);
     audioSource.setLoop(true);
     audioSource.setVolume(0.5);
@@ -124,4 +128,140 @@ const initializeAudio = (): void => {
  * Responsible to create particles
  */
 const createParticles = (): void => {
+  const imageData = getImageData(videoInput);
+  const geometry = new THREE.Geometry();
+  // @ts-ignore
+  geometry.morphAttributes = {}; // This is necessary to avoid error.
+
+  const material = new THREE.PointsMaterial({
+    size: 1,
+    color: 0xff3b6c,
+    sizeAttenuation: false
+  });
+
+  // console.log(imageData);
+  
+  for (let y=0; y < imageData.height; y++) {
+    for (let x=0; x < imageData.width; x++) {
+        const vertex = new THREE.Vector3(
+            x - imageData.width / 2,
+            -y + imageData.height / 2,
+            0
+        );
+        geometry.vertices.push(vertex);
+    }
+  }
+
+  particles = new THREE.Points(geometry, material);
+  threeScene.add(particles);
 }
+
+const getImageData = (frame: HTMLVideoElement, useCache: boolean = true): ImageData => {
+  if(useCache && imageCache) {
+    return imageCache;
+  }
+
+  const width = frame.videoWidth;
+  const height = frame.videoHeight;
+  
+  canvasElement.width = width;
+  canvasElement.height = height;
+
+  canvasContext?.translate(width, 0);
+  canvasContext?.scale(-1, -1);
+  
+  canvasContext?.drawImage(frame, 0, 0);
+  imageCache = canvasContext?.getImageData(0, 0, videoWidth, videoHeight) as ImageData;
+
+  return imageCache;
+}
+
+const getFrequencyRangeValue = (data: Uint8Array, freqRange: number[]): number => {
+  const nyquist = 48000 / 2;
+  const lowIndex = Math.round(freqRange[0] / nyquist * data.length);
+  const highIndex = Math.round(freqRange[1] / nyquist * data.length);
+  let total = 0;
+  let numFrequencies = 0;
+
+  for (let i = lowIndex; i <= highIndex; i++) {
+      total += data[i];
+      numFrequencies += 1;
+  }
+  return (total / numFrequencies) / 255;
+};
+
+const draw = (t: any = null) => {
+  clock.getDelta();
+  // const time = clock.elapsedTime;
+
+  let r = 0, g = 0, b = 0;
+
+  // audio
+  if (audioAnalyser) {
+      // analyser.getFrequencyData() would be an array with a size of half of fftSize.
+      const data = audioAnalyser.getFrequencyData();
+
+      const bass = getFrequencyRangeValue(data, frequencyRange.bass);
+      const mid = getFrequencyRangeValue(data, frequencyRange.mid);
+      const treble = getFrequencyRangeValue(data, frequencyRange.treble);
+
+      r = bass;
+      g = mid;
+      b = treble;
+  }
+
+  // video
+  if (particles) {
+      particles.material.color.r = 1 - r;
+      particles.material.color.g = 1 - g;
+      particles.material.color.b = 1 - b;
+
+      const density = 2;
+      const useCache = parseInt(t) % 2 === 0;  // To reduce CPU usage.
+      const imageData = getImageData(videoInput, useCache);
+      for (let i = 0; i < particles.geometry.vertices.length; i++) {
+          const particle = particles.geometry.vertices[i];
+          if (i % density !== 0) {
+              particle.z = 10000;
+              continue;
+          }
+          let index = i * 4;
+          let gray = (imageData.data[index] + imageData.data[index + 1] + imageData.data[index + 2]) / 3;
+          let threshold = 300;
+          if (gray < threshold) {
+              if (gray < threshold / 3) {
+                  particle.z = gray * r * 5;
+
+              } else if (gray < threshold / 2) {
+                  particle.z = gray * g * 5;
+
+              } else {
+                  particle.z = gray * b * 5;
+              }
+          } else {
+              particle.z = 10000;
+          }
+      }
+      particles.geometry.verticesNeedUpdate = true;
+  }
+
+  renderer.render(threeScene, camera);
+
+  requestAnimationFrame(draw);
+};
+
+
+const handleResize = (): void => {
+  screenWidth = window.innerWidth;
+  screenHeight = window.innerHeight;
+
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(screenWidth, screenHeight);
+
+  camera.aspect = screenWidth / screenHeight;
+  camera.updateProjectionMatrix();
+}
+
+window.addEventListener("resize", handleResize);
+
+initialize();
